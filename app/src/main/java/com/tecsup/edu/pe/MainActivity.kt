77cbbcb947b160aa5a0d09154311a0714ec8de9b
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-04-07 02:32:38 -0500

    Initial commit

---
+++ app/src/main/java/com/tecsup/edu/pe/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.tecsup.edu.pe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Establecer una animación de transición
+        //overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+
+        //setContentView(R.layout.activity_main)
+    }
+
+
+    }
+
+
